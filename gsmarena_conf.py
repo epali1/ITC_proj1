6ec66d8ca0e2f22a274c0baaec16550ed72973c3
yair ben shaul <epali1@gmail.com>
2021-07-23 13:21:29 +0300

    Requests issue fixed:    The problem:       The webpage block ip after more than ~ 20 requests.    The Solutions:       Add request header {'User-Agent': 'Mozilla/5.0'}    The reason:       The header make the request seem more legit.
     - change time delay (1 sec) between requests.
    # TODO
     - clean the code
     - add logger function (file\stdout)
     - improve table extraction.
     - extract more data from webpage:
        * numbers of likes.
        * number of comments.
        * popularity(hits)

--- gsmarena_conf.py
+++ gsmarena_conf.py
@@ -5,6 +5,7 @@ This is config file for "GSMarena" scraper.
 MAIN_SITE = 'https://www.gsmarena.com'
 SEARCH_PAGE = '/search.php3?nYearMin={}&nYearMax={}&sMakers={}&sAvailabilities={}&sFormFactors={}'
 RESULTS_PAGE = '/results.php3?nYearMin={}&nYearMax={}&sMakers={}&sAvailabilities={}&sFormFactors={}'
+BRANDS = {'Samsung': 9, 'Apple': 48}
 APPLE = '48' # Makerid
 SAMSUNG = '9' # Makerid
 AVAILABLE = '1,3' # 1 - Available, 3 - Discontinued
@@ -13,6 +14,20 @@ YEAR_RANGE = range(2009, 2022)
 # &nbsp  - HTML entity which indicate " space that will not break into a new line"
 # https://www.w3schools.com/html/html_entities.asp
 NON_BREAK_SPACE = u'\xa0'
-REQUEST_DELAY = 3  # seconds
+REQUEST_BATCH_DELAY = 60  # seconds
+BATCH_SIZE = 10
+REQUEST_SINGLE_DELAY = 1
+
+# REQUEST_HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0)'
+#                                 ' Gecko/20100101 Firefox/47.0'}
+REQUEST_HEADER = {'User-Agent': 'Mozilla/5.0'}
 # test
 # print(MAIN_SITE + RESULTS_PAGE.format(2009, 2009, APPLE, AVAILABLE, FORM_FACTOR))
+
+# request parameters
+# https://realpython.com/python-requests/#other-http-methods
+# more issues:
+# https://stackoverflow.com/questions/47397919/python-requests-with-httpadapter-is-halting-for-hours
+# https://docs.python-requests.org/en/master/user/advanced/
+GSMARENA_MAX_RETRIES = 3  # number of retry the same request before raise ConnectionError
+GSMARENA_TIMEOUT = 3  # number of seconds to wait on a response before timing out
--- phone_scraper.py
+++ phone_scraper.py
@@ -2,18 +2,22 @@
 ITC project #1: smart-phone scraper.
 This is a Data-Mining project.
 We scrape "GSMarena" website for smartphone data, of two companies: Samsung and Apple.
-
 handle robots:
 https://docs.python.org/3/library/urllib.robotparser.html
 """
 
 import requests
+from requests.adapters import HTTPAdapter
+from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup
 import json
 from gsmarena_conf import *
 from time import sleep
+from random import random
 
 JSON_INDENT = 4
+GSMARENA_ADAPTER = HTTPAdapter(max_retries=GSMARENA_MAX_RETRIES)
+GSMARENA_ADAPTER.max_retries.respect_retry_after_header = False  # abort site retry command
 
 
 def save_data(filename, data):
@@ -24,30 +28,51 @@ def save_data(filename, data):
         json.dump(data, f, ensure_ascii=False, indent=JSON_INDENT)
 
 
-def get_pages(urls: list[str], timeout=REQUEST_DELAY) -> list[requests.models.Response]:
+def get_pages(urls: list[str], batch_size=BATCH_SIZE,
+              timeout_batch=REQUEST_BATCH_DELAY, timeout_single=REQUEST_SINGLE_DELAY) -> list[requests.models.Response]:
     """
     :inputs: list of url addresses, number of threads
     :return: list of responses
-    how to handles connection errors. source:
-    https://realpython.com/python-requests/#other-http-methods
-    more issues:
-    https://stackoverflow.com/questions/47397919/python-requests-with-httpadapter-is-halting-for-hours
     """
+    # send request header to fool not detectors
+    # https://stackoverflow.com/questions/47956527/how-do-websites-detect-bots
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent/Firefox
+    # https://stackoverflow.com/questions/10606133/sending-user-agent-using-requests-library-in-python
+    # https://hacks.mozilla.org/2013/09/user-agent-detection-history-and-checklist/
+    tmp_session = requests.Session()
+    tmp_session.mount(prefix=MAIN_SITE, adapter=GSMARENA_ADAPTER)
     responses = []
+    # sleep_flag = 0
     for idx, url in enumerate(urls):
+        # print(url)
+        # exit()
+        # sleep_flag += 1
+        # if batch_size < sleep_flag:
+        #     print(f"batch sleep: {timeout_batch} sec.")
+        #     # sleep(timeout_batch)
+        #     sleep_flag = 0
         try:
-            responses.append(requests.get(url))
-        except requests.exceptions.ConnectionError as e:
-            print(e, f"\n sleep for {timeout} seconds and try again")
-            sleep(timeout)
-            responses.append(requests.get(url))
-
-        sleep(timeout)
-        print(f"num_reqs: {idx}/{len(urls)}, "
+            tmp_response = tmp_session.get(url, timeout=GSMARENA_TIMEOUT, headers=REQUEST_HEADER)
+            # tmp_response = requests.get(url, headers=REQUEST_HEADER)
+        except ConnectionError as e:
+            print(e)
+            print(f'ConnectionError: skip {url}')
+        else:
+            if not tmp_response.ok:
+                print(f'ResponseError: skip {url}')
+                continue
+            responses.append(tmp_response)
+        sleep_time = timeout_single  # + random() * timeout_single
+        print(f"num_reqs: {idx + 1}/{len(urls)}, "
               f"status: {responses[-1].ok}, "
-              f"sleep: {timeout} sec.")
-        if not responses[-1].ok:
-            exit(1)
+              f"sleep: {sleep_time} sec.")
+        # sleep between requests
+        sleep(sleep_time)
+    # if sleep_flag > 0:
+    #     print(f"batch sleep: {timeout_batch} sec.")
+        # sleep(timeout_batch)
+        # sleep_flag = 0
 
     return responses
 
@@ -94,14 +119,14 @@ def smartphones_scraper(brand: str) -> list[dict]:
     """
     scrape all iphone smartphone data from GSMarena and save data as json.
     """
-    samsung_search = [MAIN_SITE + RESULTS_PAGE.format(year, year, brand, AVAILABLE, FORM_FACTOR)
+    website_search = [MAIN_SITE + RESULTS_PAGE.format(year, year, BRANDS['Samsung'], AVAILABLE, FORM_FACTOR)
                       for year in YEAR_RANGE]
-    samsung_search_pages = get_pages(samsung_search)
-    samsung_links = get_phone_list(samsung_search_pages)
+    smartphones_search_pages = get_pages(website_search)
+    smartphones_links = get_phone_list(smartphones_search_pages)
 
     # get smartphones data
-    samsung_pages = get_pages(samsung_links)
-    smartphones_data = [get_phone_data(page) for page in samsung_pages if page.ok]
+    smartphones_pages = get_pages(smartphones_links[:100])
+    smartphones_data = [get_phone_data(page) for page in smartphones_pages if page.ok]
 
     print(f"got all {brand} smartphones data")
     return smartphones_data
@@ -110,7 +135,7 @@ def smartphones_scraper(brand: str) -> list[dict]:
 def main():
     # phones_data = smartphones_scraper(brand=APPLE)
     # save_data('apple_smartphones_data.json', phones_data)
-    phones_data = smartphones_scraper(brand=SAMSUNG)
+    phones_data = smartphones_scraper(brand='Samsung')
     save_data('samsung_smartphones_data.json', phones_data)
 
 
