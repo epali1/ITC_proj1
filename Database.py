629a2c980152a0cc1d0694c1fcd9988928f2c392
yair ben shaul <epali1@gmail.com>
2021-11-03 22:30:32 +0200

    fix all databases bugs. functional state

--- Database.py
+++ Database.py
@@ -5,6 +5,8 @@ import pymysql as mysql
 DB_SCRIPT = 'Smartphone-Scraper2.sql'
 DB_USER = 'root'
 DB_PASS = '12345COM'
+TABLES_LIST = ('Network', 'Body', 'Display', 'Selfie camera', 'Main Camera', 'Misc', 'Comms', 'Platform',
+               'Launch', 'Features', 'Sound', 'Battery', 'Memory')
 
 
 def run_query(x, y):
@@ -65,128 +67,215 @@ def to_database(data):
     # ##### Get Other Attributes and Saving on DB ######
     for i in range(0, len(file_data)):
         data_from_file = file_data[i]
-        #=> Networks Table Data 
-        Network = data_from_file[mobile_models[i]]['Network']
-        try   : Technology   = Network['Technology'] 
-        except: Technology   = ""
-        try   : twoG_Bands   = Network['2G bands']   
-        except: twoG_Bands   = ""
-        try   : threeG_Bands = Network['3G bands']   
-        except: threeG_Bands = ""
-        try   : fourG_Bands  = Network['4G bands']   
-        except: fourG_Bands  = ""
-        try   : fiveG_Bands  = Network['5G bands']   
-        except: fiveG_Bands  = ""
-        try   : Speed        = Network['Speed'] 
-        except: Speed = ""
-        #=> Body  
-        Body = data_from_file[mobile_models[i]]['Body']
-        try   : Dimensions  = Body['Dimensions'] 
-        except: Dimensions  = ""
-        try   : Weight      = Body['Weight'] 
-        except: Weight      = ""
-        try   : Build       = Body['Build'] 
-        except: Build       = ""
-        try   : Sim         = Body['SIM'] 
-        except: Sim         = ""  
-        #=> Display  
-        Display = data_from_file[mobile_models[i]]['Display']
-        try    : Type  = Display['Type'] 
-        except : Type  = ""  
-        try    : Size  = Display['Size']
-        except : Size = ""
-        try    : Resolution = Display['Resolution']  
-        except : Resolution = ""
+        #=> Networks Table Data
+        try:
+            Network = data_from_file[mobile_models[i]]['Network']
+            try   : Technology   = Network['Technology']
+            except: Technology   = ""
+            try   : twoG_Bands   = Network['2G bands']
+            except: twoG_Bands   = ""
+            try   : threeG_Bands = Network['3G bands']
+            except: threeG_Bands = ""
+            try   : fourG_Bands  = Network['4G bands']
+            except: fourG_Bands  = ""
+            try   : fiveG_Bands  = Network['5G bands']
+            except: fiveG_Bands  = ""
+            try   : Speed        = Network['Speed']
+            except: Speed = ""
+        except:
+            Technology   = ""
+            twoG_Bands = ""
+            threeG_Bands = ""
+            fourG_Bands = ""
+            fiveG_Bands = ""
+            Speed = ""
+
+        #=> Body
+        try:
+            Body = data_from_file[mobile_models[i]]['Body']
+            try   : Dimensions  = Body['Dimensions']
+            except: Dimensions  = ""
+            try   : Weight      = Body['Weight']
+            except: Weight      = ""
+            try   : Build       = Body['Build']
+            except: Build       = ""
+            try   : Sim         = Body['SIM']
+            except: Sim         = ""
+        except:
+            Dimensions  = ""
+            Weight = ""
+            Build = ""
+            Sim = ""
+
+            #=> Display
+        try:
+            Display = data_from_file[mobile_models[i]]['Display']
+            try    : Type  = Display['Type']
+            except : Type  = ""
+            try    : Size  = Display['Size']
+            except : Size = ""
+            try    : Resolution = Display['Resolution']
+            except : Resolution = ""
+        except:
+            Type  = ""
+            Size = ""
+            Resolution = ""
+
         #=> Selfie Camera
-        Selfie_Cam = data_from_file[mobile_models[i]]['Selfie camera']
-        try    : Single_1  = Selfie_Cam['Single'] 
-        except : Single_1= ""  
-        try    : Features_1 = Selfie_Cam['Features']
-        except : Features_1 = ""
-        try    : Video_1 = Selfie_Cam['Video']  
-        except : Video_1 = ""
-        #=> Main Camera
-        Main_Cam = data_from_file[mobile_models[i]]['Main Camera']
-        try    : Single_2  = Selfie_Cam['Single'] 
-        except : Single_2 = ""  
-        try    : Features_2 = Selfie_Cam['Features']
-        except : Features_2 = ""
-        try    : Video_2 = Selfie_Cam['Video']  
-        except : Video_2 = ""
+        try:
+            Selfie_Cam = data_from_file[mobile_models[i]]['Selfie camera']
+            try    : Single_1  = Selfie_Cam['Single']
+            except : Single_1= ""
+            try    : Features_1 = Selfie_Cam['Features']
+            except : Features_1 = ""
+            try    : Video_1 = Selfie_Cam['Video']
+            except : Video_1 = ""
+        except:
+            Single_1= ""
+            Features_1 = ""
+            Video_1 = ""
+
+            #=> Main Camera
+        try:
+            Main_Cam = data_from_file[mobile_models[i]]['Main Camera']
+            try    : Single_2  = Main_Cam['Single']
+            except : Single_2 = ""
+            try    : Features_2 = Main_Cam['Features']
+            except : Features_2 = ""
+            try    : Video_2 = Main_Cam['Video']
+            except : Video_2 = ""
+        except:
+            Single_2 = ""
+            Features_2 = ""
+            Video_2 = ""
+
         #=> Misc
-        Misc = data_from_file[mobile_models[i]]['Misc']
-        try    : Colors  = Misc['Colors'] 
-        except : Colors  = ""  
-        try    : Model = Misc['Models']
-        except : Model = ""
-        try    : SAR = Misc['SAR']  
-        except : SAR = ""  
-        try    : SAR_EU = Misc['SAR EU']  
-        except : SAR_EU  = "" 
-        try    : Price = Misc['Price']  
-        except : Price = "" 
+        try:
+            Misc = data_from_file[mobile_models[i]]['Misc']
+            try    : Colors  = Misc['Colors']
+            except : Colors  = ""
+            try    : Model = Misc['Models']
+            except : Model = ""
+            try    : SAR = Misc['SAR']
+            except : SAR = ""
+            try    : SAR_EU = Misc['SAR EU']
+            except : SAR_EU  = ""
+            try    : Price = Misc['Price']
+            except : Price = ""
+        except:
+            Colors  = ""
+            Model = ""
+            SAR = ""
+            SAR_EU = ""
+            Price = ""
+
         #=> Comms
-        Comms = data_from_file[mobile_models[i]]['Comms']
-        try    : Wlan  = Comms['WLAN'] 
-        except : Wlan  = ""  
-        try    : Bluetooth = Comms['Bluetooth']
-        except : Bluetooth = ""
-        try    : GPS = Comms['GPS']  
-        except : GPS = ""  
-        try    : NFC = Comms['NFC']  
-        except : NFC  = "" 
-        try    : Radio = Comms['Radio']  
-        except : Radio = "" 
-        try    : Usb = Comms['USB']  
-        except : Usb = "" 
+        try:
+            Comms = data_from_file[mobile_models[i]]['Comms']
+            try    : Wlan  = Comms['WLAN']
+            except : Wlan  = ""
+            try    : Bluetooth = Comms['Bluetooth']
+            except : Bluetooth = ""
+            try    : GPS = Comms['GPS']
+            except : GPS = ""
+            try    : NFC = Comms['NFC']
+            except : NFC  = ""
+            try    : Radio = Comms['Radio']
+            except : Radio = ""
+            try    : Usb = Comms['USB']
+            except : Usb = ""
+        except:
+            Wlan  = ""
+            Bluetooth = ""
+            GPS = ""
+            NFC = ""
+            Radio = ""
+            Usb = ""
+
         #=> Platform
-        Platform = data_from_file[mobile_models[i]]['Platform']
-        try    : OS  = Platform['OS'] 
-        except : OS  = ""  
-        try    : Chipset = Platform['Chipset']
-        except : Chipset = ""
-        try    : CPU = Platform['CPU']  
-        except : CPU = ""  
-        try    : GPU = Platform['GPU']  
-        except : GPU  = "" 
-        try    : Radio = Platform['Radio']  
-        except : Radio = "" 
-        try    : USb = Platform['USB']  
-        except : USb = "" 
-        #=> Launch 
-        Launch = data_from_file[mobile_models[i]]['Launch']
-        try    : Annou = Launch['Announced']  
-        except : Annou = "" 
-        try    : Status = Launch['Status']  
-        except : Status = "" 
-        #=> Features 
-        Features = data_from_file[mobile_models[i]]['Features']
-        try    : Sensors =str(Features['Sensors'] )
-        except : Sensors = "" 
-        try    : other = Features['other']  
-        except : other = ""     
+        try:
+            Platform = data_from_file[mobile_models[i]]['Platform']
+            try    : OS  = Platform['OS']
+            except : OS  = ""
+            try    : Chipset = Platform['Chipset']
+            except : Chipset = ""
+            try    : CPU = Platform['CPU']
+            except : CPU = ""
+            try    : GPU = Platform['GPU']
+            except : GPU  = ""
+            try    : Radio = Platform['Radio']
+            except : Radio = ""
+            try    : USb = Platform['USB']
+            except : USb = ""
+        except:
+            OS  = ""
+            Chipset = ""
+            CPU = ""
+            GPU = ""
+            Radio = ""
+            USb = ""
+
+        #=> Launch
+        try:
+            Launch = data_from_file[mobile_models[i]]['Launch']
+            try    : Annou = Launch['Announced']
+            except : Annou = ""
+            try    : Status = Launch['Status']
+            except : Status = ""
+        except:
+            Annou = ""
+            Status = ""
+
+        #=> Features
+        try:
+            Features = data_from_file[mobile_models[i]]['Features']
+            try    : Sensors =str(Features['Sensors'] )
+            except : Sensors = ""
+            try    : other = Features['other']
+            except : other = ""
+        except:
+            Sensors = ""
+            other = ""
+
         #=> Sound
-        Sound  = data_from_file[mobile_models[i]]['Sound']
-        try    : Loud_Spk = Sound['Loudspeaker ']  
-        except : Loud_Spk = "" 
-        try    : Jack = Sound['3.5mm jack']  
-        except : Jack = ""      
+        try:
+            Sound  = data_from_file[mobile_models[i]]['Sound']
+            try    : Loud_Spk = Sound['Loudspeaker ']
+            except : Loud_Spk = ""
+            try    : Jack = Sound['3.5mm jack']
+            except : Jack = ""
+        except:
+            Loud_Spk = ""
+            Jack = ""
+
         #=> Battary
-        Battery = data_from_file[mobile_models[i]]['Battery']
-        try    : Type = Battery['Type']  
-        except : Type = "" 
-        try    : Standby = Battery['Stand-by']  
-        except : Standby = ""  
-        try    : Talk_time = Battery['Talk time']  
-        except : Talk_time = "" 
+        try:
+            Battery = data_from_file[mobile_models[i]]['Battery']
+            try    : Type = Battery['Type']
+            except : Type = ""
+            try    : Standby = Battery['Stand-by']
+            except : Standby = ""
+            try    : Talk_time = Battery['Talk time']
+            except : Talk_time = ""
+        except:
+            Type = ""
+            Standby = ""
+            Talk_time = ""
+
         #=> Memory
-        Memory = data_from_file[mobile_models[i]]['Memory']
-        try    : Card_slot = Battery['Card slot']  
-        except : Card_slot = "" 
-        try    : Internal = Battery['Internal']  
-        except : Internal = ""  
-        try    : Other = Battery['other']  
-        except : Other = ""
+        try:
+            Memory = data_from_file[mobile_models[i]]['Memory']
+            try    : Card_slot = Memory['Card slot']
+            except : Card_slot = ""
+            try    : Internal = Memory['Internal']
+            except : Internal = ""
+            try    : Other = Memory['other']
+            except : Other = ""
+        except:
+            Card_slot = ""
+            Internal = ""
+            Other = ""
+
         # Query 1 : Battery
         a = '''INSERT INTO `battary`(`Type`, `Charging`) VALUES (%s,%s)'''
         b = (Type, None)
@@ -212,10 +301,9 @@ def to_database(data):
         b = (Annou, Status)
         run_query(a, b)
         # Query 7 : Main Camera
-        a= '''INSERT INTO `main_camera`(`Quad`, `Features`, `Video`) VALUES (%s,%s,%s)'''
-        b= (Single_2, Features_2, Video_2)
+        a = '''INSERT INTO `main_camera`(`Quad`, `Features`, `Video`) VALUES (%s,%s,%s)'''
+        b = (Single_2, Features_2, Video_2)
         run_query(a, b)
-
         # Query 8 : Memory
         a = '''INSERT INTO `memory`(`Card_slot`, `Internal`, `other`) VALUES (%s,%s,%s)'''
         b = (Card_slot, Internal, Other)
@@ -234,7 +322,7 @@ def to_database(data):
         run_query(a, b)
         # Query 12 : Selfie Camera
         a = '''INSERT INTO `selfie_camera`(`Single`, `features`, `Video`) VALUES (%s,%s,%s)'''
-        b = (Single_1,Features_1,Video_1)
+        b = (Single_1, Features_1, Video_1)
         run_query(a, b)
         # Query 13 : Sound
         a = '''INSERT INTO `sound`(`Loudspeaker`, `3.5mm_jack`) VALUES (%s,%s)'''
@@ -247,8 +335,7 @@ def to_database(data):
         (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
         name_p = mobile_models[i]
         i = i + 1
-        
-        b= (name_p, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i)
+        b = (name_p, i, i, i, i, i, i, i, i, i, i, i, i, i, i, i)
         run_query(a, b)
 
 
